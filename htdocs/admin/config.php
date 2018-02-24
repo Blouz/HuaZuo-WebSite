b80655b5f7bd5120ade1c0f8029e8bfa165c5f65
francisafu <francis_afu@163.com>
2018-02-24 18:36:53 +0800

    First Upload

---
+++ htdocs/admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://myhuazuo.com/admin/');
+define('HTTP_CATALOG', 'http://myhuazuo.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://myhuazuo.com/admin/');
+define('HTTPS_CATALOG', 'http://myhuazuo.com/');
+
+// DIR
+define('DIR_APPLICATION', '/opt/lampp/htdocs/admin/');
+define('DIR_SYSTEM', '/opt/lampp/htdocs/system/');
+define('DIR_IMAGE', '/opt/lampp/htdocs/image/');
+define('DIR_STORAGE', '/opt/lampp/storage/');
+define('DIR_CATALOG', '/opt/lampp/htdocs/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'HuaZuoDB');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
